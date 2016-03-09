c44dffcc18250c4857f6e7b93c612738e5f7e56d
Todd Eidson <teidson@bapm.com>
2016-03-09 15:12:51 -0500

    Added SemanticReportsExtension.php

--- DependencyInjection/SemanticReportsExtension.php
+++ DependencyInjection/SemanticReportsExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */;
-class EidsonatorReportsExtension extends Extension
+class SemanticReportsExtension extends Extension
 {
     /**
      * {@inheritdoc}
